da058b63630e690ae98e8846e957efad8915e364
CarlosVeg <carlosvegapro@gmail.com>
2021-02-04 01:00:55 -0600

    Agregando directivas y estilos al proyecto

--- Directivas/src/app/app.component.ts
+++ Directivas/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Directivas';
+  listStudents = [
+    {nombre: "Carlos", estado: "Regular"},
+    {nombre: "Clara", estado: "Nelson"},
+    {nombre: "Antonio", estado: "Regular"},
+    {nombre: "Nidia", estado: "Nelson"},
+    {nombre: "Nicolas", estado: "Libre"},
+  ];
+  mostrar = true;
+
+  toogle(): void {
+    this.mostrar = !this.mostrar;
+  }
 }
