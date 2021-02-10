bed4d8a1a57d4d42c60000544d43620a293e3d09
CarlosVeg <carlosvegapro@gmail.com>
2021-02-09 23:17:43 -0600

    Funcion de conversion agregada

--- Convertidor/src/app/components/convertidor/convertidor.component.ts
+++ Convertidor/src/app/components/convertidor/convertidor.component.ts
@@ -10,9 +10,31 @@ export class ConvertidorComponent implements OnInit {
   origen: string = "USD";
   destino: string = "EUR";
   total: number = 0;
+  monedas: string[] = ["USD", "EUR", "LIBRA"];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  convertir(): void {
+    switch(this.origen) {
+      case "USD":
+        if (this.destino === "USD") this.total = this.cantidad;
+        if (this.destino === "EUR") this.total = this.cantidad * 0.82;
+        if (this.destino === "LIBRA") this.total = this.cantidad * 0.72;
+        break;
+      case "EUR":
+        if (this.destino === "USD") this.total = this.cantidad * 1.21;
+        if (this.destino === "EUR") this.total = this.cantidad;
+        if (this.destino === "LIBRA") this.total = this.cantidad * 0.88;
+        break;
+      case "LIBRA":
+        if (this.destino === "USD") this.total = this.cantidad * 1.38;
+        if (this.destino === "EUR") this.total = this.cantidad * 1.14;
+        if (this.destino === "LIBRA") this.total = this.cantidad;
+        break;
+    }
   }
 
 }
